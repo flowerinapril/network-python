a0c84735307b487cd14922ea0ad3bba2e4c1c563
StarkYue <35569001+flowerinapril@users.noreply.github.com>
2020-10-29 17:23:27 +0800

    Add files via upload

---
+++ CODE/socket04.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+import socket
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.settimeout(0.5)
+ip = "19.244.46.22"
+for port in range(5000,9000):
+    result = s.connect_ex((ip, port))
+    if result ==0:
+        print('port %d is opened' % port)
+s.close()
